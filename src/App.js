07e21b4c26b63b03b320c727a66d103ac8d043ab
gyscoder <geysomsilva.20@gmail.com>
2022-07-07 21:13:21 -0300

    criação e implementação da pasta componentes e do navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import React, { useState } from "react";
 
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
-    <div className="App">
+    <div className="App-container">
+      <Navbar />
+      <div className="registrar-livros">
+        <h1></h1>
+      </div>
     </div>
   );
 }
