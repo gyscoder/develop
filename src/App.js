510f4b4fbd9142ad9137900aae476a87831d917c
gyscoder <geysomsilva.20@gmail.com>
2022-07-07 23:31:19 -0300

    alteração no front end, css modificado, uma base para o resto da modelagem criada

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 // import React, { useState } from "react";
 
 import Navbar from "./components/Navbar/Navbar";
-import Sexo from "./components/TaskItem/registrarLivros";
+import Livros from "./components/TaskItem/registrarLivros";
 
 function App() {
   return (
     <div className="App-container">
       <Navbar />
-      
       <div className="registrar-livros">
-      <Sexo />
+        <div className="ajuste">
+        <Livros />
+        </div>
       </div>
     </div>
   );
--- src/components/TaskItem/registrarLivros.js
+++ src/components/TaskItem/registrarLivros.js
@@ -5,12 +5,17 @@ import "./task-item.css";
 export default function registrarLivros() {
     return (
         <div className="task-item">
+            <h1 className="adicionar-livro">Adicionar Livro</h1>
             <input type="text" name="titulo" placeholder="Título" className="register-input" />
             <input type="text" name="autor" placeholder="Autor" className="register-input" />
             <input type="date" name="data-inicio" placeholder="Data de hoje" className="register-input" />
             <input type="date" name="data-final" placeholder="Data conclusão" className="register-input" />
             <input type="text" name="nota" placeholder="Nota" className="register-input" />
-            <input type="text" name="autor" placeholder="Status" className="register-input" />
+            <select className="register-input">
+                <option className="register-input" value="Quero-ler">Quero Ler</option>
+                <option className="register-input" value="Lendo">Lendo</option>
+                <option className="register-input" value="Lido">Lido</option>
+            </select>
             <button className="register-button">Cadastrar Livro</button>
         </div>
     );
